4b1800c9e00f3b5686bc7b447b0ecb20ae92c972
gloriaJun <pureainu@gmail.com>
2019-09-18 02:09:13 +0900

    chore: Modify eslint configuration and build script to fix build error [#5]

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,14 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-  env: {
-    browser: true,
-    es6: true,
-    jest: true,
-  },
   extends: [
+    // 'react-app',
     'airbnb',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
-  plugins: ['react', '@typescript-eslint', 'prettier', 'react-hooks'],
+  plugins: ['@typescript-eslint', 'prettier', 'react-hooks'],
   rules: {
     'prettier/prettier': 'error',
 
@@ -24,18 +17,31 @@ module.exports = {
     camelcase: 'error',
     'no-param-reassign': ['error', { props: false }],
 
-    // TypeScript
+    /**
+     * TypeScript
+     */
     '@typescript-eslint/no-explicit-any': 0,
     // '@typescript-eslint/indent': ['error', 2],
     // '@typescript-eslint/prefer-interface': 'off',
+    "@typescript-eslint/no-angle-bracket-type-assertion": "off",
 
-    // React
+    /**
+     * React
+     */
     'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
     // 'react/prop-types': 'off',
 
     // React-hooks
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
   },
   settings: {
     react: {
