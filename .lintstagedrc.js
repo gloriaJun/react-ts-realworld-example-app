d6f570dea40642e175db92f46ff5fb28b98a59af
gloriaJun <pureainu@gmail.com>
2021-06-30 09:38:16 +0900

    chore: configure lint-stage

---
+++ .lintstagedrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	'*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write', 'git add'],
+	'*.{ts,tsx}': [
+	  () => 'tsc --skipLibCheck --noEmit',
+	  'eslint --fix',
+	  'prettier --write',
+	  'git add',
+	],
+	'{*.{json,md}}': ['prettier --write', 'git add'],
+      };
