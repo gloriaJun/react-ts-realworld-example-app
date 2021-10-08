5e485a8c735bf5d0d0164d467bf34794dfab5ce9
gloriaJun <pureainu@gmail.com>
2021-10-08 11:42:05 +0900

    feat: add routing

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  HOME: '/',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  SETTING: '/settings',
+  EDITOR: '/editor',
+  ARTICLE: '/article',
+  PROFILE: '/profile',
+};
---
+++ src/pages/Editor/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Editor() {
+  return (
+    <p>
+      Editor page to create/edit articles (URL: /#/editor,
+      /#/editor/article-slug-here )
+    </p>
+  );
+}
